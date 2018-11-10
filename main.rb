797540fb4617ff342f2509760dcdd04ee6cd99a2
alex <alex_is_ok@yahoo.com>
2018-11-10 21:00:21 +0200

    carcass basic logic added

--- main.rb
+++ main.rb
@@ -3,10 +3,15 @@ require 'erb'
 
 class App
   def call(env)
-    request = Rack::Request.new(env)
-    case request.path
+    @request = Rack::Request.new(env)
+    case @request.path
     when '/'
       then Rack::Response.new(render('index.html.erb'))
+    when '/game'
+      Rack::Response.new do |response|
+        response.set_cookie('pet_name', @request.params['name'])
+        Rack::Response.new(render('game.html.erb'))  # render page ??
+      end
       else Rack::Response.new('Not found', 404)
     end
   end
@@ -15,4 +20,9 @@ class App
     path = File.expand_path("../templates/#{template}", __FILE__)
     ERB.new(File.read(path)).result(binding)
   end
+
+  def pet_name
+    @request.cookies['pet_name']
+  end
+
 end
